6f52d1b65632b2acb7ca973e190e8aea2a9b9f91
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2024-12-30 17:12:01 +0300

    updating 268. Missing Number

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,8 +1,10 @@
 class Solution:
-    def missingNumber(self, nums: list[int]) -> int:
-        if (len(nums) == 0 or len(nums) == 1):
-            return 0
+    def missingNumber(self, nums: List[int]) -> int:
         nums = sorted(nums)
+
+        if (nums[0]!= 0):
+            return 0
+
         current_num=nums[0]
         for num in nums[1:]:
             if num != current_num+1:
