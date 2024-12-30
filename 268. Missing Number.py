9f9c97ec91ee4794af87d814c3f04443ba3984e4
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2024-12-30 17:24:58 +0300

    Optimiza time capacity for 268. Missing Number

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,17 +1,12 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        nums = sorted(nums)
+    def missingNumber(self, nums: list[int]) -> int:
+        sumOfNums= (len(nums) * (len(nums)+1))/2
+        current_sum =0
+        for num in nums:
+            current_sum += num
 
-        if (nums[0]!= 0):
-            return 0
-
-        current_num=nums[0]
-        for num in nums[1:]:
-            if num != current_num+1:
-                return current_num+1
-            current_num = num
-        return nums[len(nums)-1]+1
+        return int(sumOfNums- current_sum)
                 
 solution = Solution()
-result = solution.missingNumber([1, 3, 4, 5, 6, 7])
+result = solution.missingNumber([0, 1, 3, 4, 5])
 print(result)
