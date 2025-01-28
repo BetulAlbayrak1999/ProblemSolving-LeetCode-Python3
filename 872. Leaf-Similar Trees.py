dd4ff1ef633028f23f274c2696cebeca3130de9b
BetulAlbayrak1999 <betul.1999sytur@gmail.com>
2025-01-28 11:25:46 +0300

    [200~872. Leaf-Similar Trees

---
+++ 872. Leaf-Similar Trees.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+class Solution:
+    def leafSimiler(self, root1: TreeNode, root2: TreeNode)-> bool:
+        def getLeaves(root: TreeNode):
+            if not root:
+                return []
+            if not root.left and not root.right:
+                return [root.val]
+            return getLeaves(root.left) + getLeaves(root.right)
+
+        leaves1= getLeaves(root1)
+        leaves2= getLeaves(root2)
+
+        return leaves1== leaves2
+
+
+root1= TreeNode (3)
+root1.left= TreeNode(5, TreeNode(6), TreeNode(3, TreeNode(7), TreeNode(4)))
+root1.right = TreeNode (1, None, TreeNode(9))
+
+root2= TreeNode(5)
+root2.left= TreeNode(6)
+root2.right= TreeNode(1, TreeNode(2, TreeNode(7), TreeNode(4)), TreeNode(9))
+
+solution =Solution ()
+print(solution.leafSimiler(root1, root2))
--- temp.py
+++ temp.py
@@ -1,44 +1,24 @@
-# name = "betul"
-# age = 26
-# rank = 10
+dicSkills= {
+    "Osama": {
+        "css": "30",
+        "html":"10",
+        "python": "94"
+    },
+    "Betul": {
+        "css": "30",
+        "html":"10",
+        "python": "94"
+    },
+    "Rasha": {
+        "css": "30",
+    "html":"10",
+    "python": "94"
+    }
+}
+for key, value in dicSkills.items():
+    print(key)
+    for key, value in value.items():
+        print(key, value)
+    print("\n")
 
-# print("my name is: {:s} and Rank is: {:d}".format(name, age))
-
-# myMoney = 62378272173092137
-# print("\nMy Money in the bank is: {:d}".format(myMoney))
-# print("\nMy Money in the bank is: {:,d}".format(myMoney))
-
-# a, b, c = "one", "two", "three"
-# print("sorted as {2} {1} {0}".format(a, b, c))
-
-# print(int(10.6))
-
-# listOne = ["osama", "hamdan", "kareem", 3, 4, 3.4, 3, 3]
-# listTwo = [1111, "second list"]
-# listOne.append(listTwo)
-# print(listOne[8][1])
-
-# listOne.extend(listTwo)
-# # listOne.sort(reverse=True)
-
-# listOne.reverse()
-# listOne.insert(3, "check insert")
-# print(listOne)
-# print(listOne.pop(3))
-# myTuple = ("betul",)
-# print(type(myTuple))
-
-# a = ("a", "b", "c", "d")
-# x, y, _, z = a
-# print(x, y, z)
-# # in set{}; items unique and not sorted
-
-# q = {1, 3, 4, "one", "two"}
-# w = {5, 7, 8, 23, 1, 1, 1}
-
-# print(q | w)
-# print(q.union(w, q))
-# # pop is removing random element
-
-email = "betulalbayrak1999@gmail.com"
-print(email[0 : email.index("@")])
+print("Thanks")
